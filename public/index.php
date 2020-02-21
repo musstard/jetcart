6c38b3a854a14ba7ecafb68ae00d626c12770eab
musstard <muss@muss.co.nz>
2020-02-21 15:21:06 +1300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,19 @@
 <?php
 
-echo "Is bob.php there?"
+echo "Is bob.php there?";
+
+if ($handle = opendir('/var/www/')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry\n";
+        }
+    }
+
+    closedir($handle);
+}
 
 phpinfo();
 
